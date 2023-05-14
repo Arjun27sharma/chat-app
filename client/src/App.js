b148a68fb1e16c80219c477458c48dc50d8680aa
Arjun Sharma <arjunsharma6622@gmail.com>
2023-05-15 00:36:48 +0530

    ui modification

--- client/src/App.js
+++ client/src/App.js
@@ -25,17 +25,29 @@ function App() {
 
       {!showChat ? (
 
+        <div className='chatMain'>
+
+          <div className="details">
+            <h4>How to use this app?</h4>
+            <span>1. Enter Your Name</span>
+            <span>2. Enter some Room ID</span>
+            <span>3. Give your Friend the same Room ID</span>
+          
+          </div>
+
         <div className="joinChatContainer">
 
-          <h3>Join A Chat</h3>
+          <h3>Join a <span>CHAT</span></h3>
           <input type="text" placeholder="Name..." onChange={(e) => setUsername(e.target.value)} />
           <input type="text" placeholder="Room ID (ex : 1234)..." onChange={(e) => setRoom(e.target.value)} />
           <button onClick={joinRoom}>Join the Rooom</button>
         </div>
+        </div>
 
       ) : (
         <div>
-          <h1>{username}</h1>
+          <h1>Welcome to the room - <span style={{ color: "yellow" ,textTransform: "uppercase"}}>{username}</span></h1>
+          <h2>Room ID - <span style={{ color: "yellow" }}>{room}</span></h2>
         <Chat socket={socket} username={username} room={room} />
         </div>
       )}
